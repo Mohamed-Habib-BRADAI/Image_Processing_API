170ae6d9fa16d4694774acd85ae0e543b79bd3ce
mhbCO <mohamedhabib.bradai@componize.com>
2021-05-23 21:21:33 +0200

    implement logic

---
+++ src/utilities/logger.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+import exp = require("constants");
+
+const logger = (req: express.Request, res: express.Response, next: Function): void => {
+    let params = req.params;
+    console.log(params);
+    next();
+}
+export default  logger;
